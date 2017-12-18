4437ca2c9c979fcd82e037f5a9d96698bde2b36c
Megan Nguyen <nguyenmegan@berkeley.edu>
2017-12-18 01:04:27 -0800

    mobile-friendly

--- workoutwars/workoutwarsapp/static/js/script.js
+++ workoutwars/workoutwarsapp/static/js/script.js
@@ -1,13 +1,1 @@
-// $(document).ready(function () {
-//     $('.navbar-light .navbar-nav .nav-link').click(function(e) {
-//         console.log('aqui');
-//
-//         $('.navbar-light .navbar-nav .nav-item.active').removeClass('active');
-// console.log('aqui3');
-//         var $parent = $(this).parent();
-//         $parent.addClass('active')
-//         $(".navbar-light .navbar-nav .nav-link").load($("a").attr("href"));
-//         e.preventDefault();
-//         e.stopPropagation();
-//     });
-// });
+
