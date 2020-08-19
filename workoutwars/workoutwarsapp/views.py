f3481a51dbd8315db82fccdf265b8d3b6faf8667
Phoebe Li <liphoebe01@gmail.com>
2020-08-18 22:25:11 -0700

    change global start date

--- workoutwars/workoutwarsapp/views.py
+++ workoutwars/workoutwarsapp/views.py
@@ -16,7 +16,7 @@ from workoutwarsapp.forms import SignUpForm, AddWorkoutForm
 from workoutwarsapp.models import User, Profile, Class, Team, Exercise, Workout
 
 # Globals
-START_DATE = datetime.date(2020, 3, 28)
+START_DATE = datetime.date(2020, 8, 19)
 TODAY = datetime.date.today()
 NUM_DAYS = (TODAY - START_DATE).days + 1
 
